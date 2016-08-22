88dbff2e30cad1090fa21a1fc0c543e872ebdbf1
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-08-22 20:15:44 +0300

    Add demo.cmd

---
+++ demo.cmd
@@ -0,0 +1,54 @@
+@echo off
+
+rem Displayed buttons.
+set MB_ABORTRETRYIGNORE=2
+set MB_CANCELTRYCONTINUE=6
+set MB_OK=0
+set MB_OKCANCEL=1
+set MB_RETRYCANCEL=5
+set MB_YESNO=4
+set MB_YESNOCANCEL=3
+
+rem Icon.
+set MB_ICONEXCLAMATION=48
+set MB_ICONWARNING=48
+set MB_ICONINFORMATION=64
+set MB_ICONASTERISK=64
+set MB_ICONQUESTION=32
+set MB_ICONSTOP=16
+set MB_ICONERROR=16
+set MB_ICONHAND=16
+
+rem Default button.
+set MB_DEFBUTTON1=0
+set MB_DEFBUTTON2=256
+set MB_DEFBUTTON3=512
+set MB_DEFBUTTON4=768
+
+rem Other options.
+set MB_SETFOREGROUND=65536
+
+rem Result ERRORLEVELs.
+
+set IDABORT=3
+set IDCANCEL=2
+set IDCONTINUE=11
+set IDIGNORE=5
+set IDNO=7
+set IDOK=1
+set IDRETRY=4
+set IDTRYAGAIN=10
+set IDYES=6
+
+rem ----------------------------------------------------------------------------
+
+set /a type=%MB_YESNO% + %MB_ICONQUESTION% + %MB_DEFBUTTON2%
+messagebox "Enough message boxes?" "A serious question" %type%
+if "%ERRORLEVEL%"=="%IDNO%" goto no
+:yes
+echo No more message boxes for you, then.
+goto end
+:no
+set /a type=%MB_OK% + %MB_ICONINFORMATION%
+messagebox "Okay, here's another one!" "MessageBox the Second" %type%
+:end
