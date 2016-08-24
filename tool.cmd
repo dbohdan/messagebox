edf492681d96ac7276aa21b53eb61441338b4afd
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-08-24 12:49:43 +0300

    tool.cmd: Determine path to MinGW/MSYS at runtime
    Do so for either only if it is necessary to perform an action
    requested by the user.

--- tool.cmd
+++ tool.cmd
@@ -1,5 +1,6 @@
 @echo off
-set path=C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%path%
+set mingw_path=
+set msys_path=
 
 rem Process the command line arguments.
 set flag_batch=0
@@ -7,7 +8,6 @@ set flag_build=0
 set flag_clean=0
 set flag_test=0
 set test_args=
-set error=0
 for %%a in (%*) do (
     if "%%a"=="/batch" set flag_batch=1
     if "%%a"=="/build" set flag_build=1
@@ -16,6 +16,37 @@ for %%a in (%*) do (
     if "%%a"=="/test" set flag_test=1
 )
 
+rem Determine the path to MinGW and MSYS (as needed).
+setlocal ENABLEDELAYEDEXPANSION
+if "%flag_build%"=="1" (
+    if "%mingw_path%"=="" (
+        if exist C:\MinGW\bin (
+            set mingw_path=C:\MinGW\bin
+        ) else if exist C:\tools\mingw32\bin (
+            set mingw_path=C:\tools\mingw32\bin
+        ) else (
+            echo Error: can't find a MinGW installation.
+            echo You can set the path manually in %0.
+            goto error
+        )
+    )
+    set path=!mingw_path!;!path!
+)
+
+if "%flag_test%"=="1" (
+    if "%msys_path%"=="" (
+        if exist C:\MinGW\msys\1.0\bin (
+            set msys_path=C:\MinGW\msys\1.0\bin
+        ) else (
+            echo Error: can't find an MSYS installation.
+            echo You can set the path manually in %0.
+            goto error
+        )
+    )
+    set path=!msys_path!;!path!
+)
+
+rem Perform the requested actions.
 if "%flag_clean%"=="1" (
     del messagebox.exe *.o
     if ERRORLEVEL 1 goto error
